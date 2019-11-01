92dbc05ec1d5cb2d7096b547f40258c7ff561a82
rala72 <code@rala.io>
2019-11-01 10:50:30 +0100

    snapshot 0101

--- src/at/rala/shell/command/CommandMethodAdapter.java
+++ src/at/rala/shell/command/CommandMethodAdapter.java
@@ -2,8 +2,12 @@ package at.rala.shell.command;
 
 import at.rala.shell.Input;
 import at.rala.shell.annotation.CommandMethod;
+import at.rala.shell.exception.MethodCallException;
 
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CommandMethodAdapter implements Command {
     private final Object object;
@@ -17,9 +21,40 @@ public class CommandMethodAdapter implements Command {
     @Override
     public void execute(Input input) {
         try {
-            commandMethod.getMethod().invoke(object, input.getArguments());
+            Parameter[] parameters = commandMethod.getMethod().getParameters();
+            List<Object> objects = new ArrayList<>();
+            for (int i = 0; i < parameters.length; i++) {
+                String argument = input.get(i).orElseThrow(() -> new MethodCallException("argument missing"));
+                objects.add(mapParameter(parameters[i].getType(), argument));
+                if (parameters[i].isVarArgs())
+                    for (int j = i + 1; j < input.getArguments().size(); j++)
+                        objects.add(mapParameter(parameters[i].getType(), argument));
+            }
+            commandMethod.getMethod().invoke(object, objects.toArray());
         } catch (IllegalAccessException | InvocationTargetException e) {
-            e.printStackTrace();
+            throw new MethodCallException(e);
         }
+    }
+
+    private static Object mapParameter(Class<?> type, String value) {
+        if (boolean.class.isAssignableFrom(type) || Boolean.class.isAssignableFrom(type)) {
+            return Boolean.parseBoolean(value);
+        }
+        if (int.class.isAssignableFrom(type) || Integer.class.isAssignableFrom(type)) {
+            return Integer.parseInt(value);
+        }
+        if (long.class.isAssignableFrom(type) || Long.class.isAssignableFrom(type)) {
+            return Long.parseLong(value);
+        }
+        if (float.class.isAssignableFrom(type) || Float.class.isAssignableFrom(type)) {
+            return Float.parseFloat(value);
+        }
+        if (double.class.isAssignableFrom(type) || Double.class.isAssignableFrom(type)) {
+            return Double.parseDouble(value);
+        }
+        if (char.class.isAssignableFrom(type) || Character.class.isAssignableFrom(type)) {
+            return (value != null && value.length() == 1) ? value.charAt(0) : null;
+        }
+        return value;
     }
 }
---
+++ src/at/rala/shell/exception/MethodCallException.java
@@ -0,0 +1,11 @@
+package at.rala.shell.exception;
+
+public class MethodCallException extends RuntimeException {
+    public MethodCallException(String message) {
+        super(message);
+    }
+
+    public MethodCallException(Throwable cause) {
+        super(cause);
+    }
+}

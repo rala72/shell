820af2edcfacea6c1a926723d822c0a7ad69c8c9
rala72 <code@rala.io>
2019-11-02 23:26:07 +0100

    improve commandLoader tests

--- src/test/java/at/rala/shell/annotation/CommandLoaderTest.java
+++ src/test/java/at/rala/shell/annotation/CommandLoaderTest.java
@@ -5,10 +5,17 @@ import org.junit.jupiter.api.Test;
 
 class CommandLoaderTest {
     @Test
-    void testSimpleCommandMethodLoading() {
-        CommandLoader commandLoader = new CommandLoader(new TestObject());
+    void testCommandWithoutAttributesMethodLoading() {
+        CommandLoader commandLoader = new CommandLoader(new TestObjectWithoutAttributes());
         Assertions.assertEquals(1, commandLoader.getCommandMethodMap().size());
         Assertions.assertTrue(commandLoader.getCommandMethodMap().containsKey("commandWithoutAttributes"));
+    }
+
+    @Test
+    void testCommandWithAttributesMethodLoading() {
+        CommandLoader commandLoader = new CommandLoader(new TestObjectWithAttributes());
+        Assertions.assertEquals(1, commandLoader.getCommandMethodMap().size());
+        Assertions.assertTrue(commandLoader.getCommandMethodMap().containsKey("cmd"));
     }
 
     @Test
@@ -20,14 +27,21 @@ class CommandLoaderTest {
 
     @Test
     void testToString() {
-        CommandLoader commandLoader = new CommandLoader(new TestObject());
+        CommandLoader commandLoader = new CommandLoader(new TestObjectWithoutAttributes());
         Assertions.assertEquals("commandWithoutAttributes", commandLoader.toString());
     }
 
     @SuppressWarnings({"unused"})
-    private static class TestObject {
+    private static class TestObjectWithoutAttributes {
         @Command
         public void commandWithoutAttributes() {
+        }
+    }
+
+    @SuppressWarnings({"unused"})
+    private static class TestObjectWithAttributes {
+        @Command("cmd")
+        public void commandWithAttributes() {
         }
     }
 
